a62a0d596ea93d168e97514aeadb704e00f4fa8e
Daniel Hurtado Quero <131759030+OconnelDan@users.noreply.github.com>
2026-08-10 22:51:56 +0200

    Actualizar main.tsx
    Corrige las rutas de GitHub Pages.

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 import { registerSW } from "virtual:pwa-register";
@@ -12,8 +12,8 @@ registerSW({ immediate: true });
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <HashRouter>
+     <App />
+    </HashRouter>
   </React.StrictMode>
 );
